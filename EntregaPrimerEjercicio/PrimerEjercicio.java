144ebdb9dde1716beee26a490737aa3356adb152
Agustina Gallardo <agusgallardo275@gmail.com>
2023-01-31 11:32:59 -0300

    EntregaPrimerEjercicio

---
+++ EntregaPrimerEjercicio/PrimerEjercicio.java
@@ -0,0 +1,21 @@
+package EntregaPrimerEjercicio;
+
+public class PrimerEjercicio {
+	
+	public static void main (String[] args) {
+		
+		String nombre = "Agustina Gallardo";
+		int edad = 33;
+		long dni = 34601166l;
+		double peso = 54.3d;
+		boolean estudia = true;
+	
+		System.out.println("Mi nombre es: "+ nombre);	
+		System.out.println("Tengo " + edad + " años.");
+		System.out.println("Mi numero de dni es " + dni);
+		System.out.println("Mi peso es de: " + peso);
+		System.out.println("Estudias en la actualidad: " + estudia);
+	}
+		
+	
+}
