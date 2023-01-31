51acc51edf2096e6e12db319cf09c399ed402912
Agustina Gallardo <agusgallardo275@gmail.com>
2023-01-31 18:28:20 -0300

    TerceraEntrega

---
+++ EntregaTercerEjercicio/TerceroEjercicio.java
@@ -0,0 +1,28 @@
+package EntregaTercerEjercicio;
+
+public class TerceroEjercicio {
+	
+	public static void main (String[] args) {
+	
+	String[] nombres = {"Maria","Agustina","Jose", "Isaac"};	
+	
+	for(int i =0; i < nombres.length; i++ ) {
+		
+		System.out.println(nombres[i]);
+	}
+	
+	// Otra opcion: 
+	
+	for(String item:nombres) {
+		System.out.println(item);
+	}
+	
+	
+	
+	
+	
+	
+	
+  }
+
+}
