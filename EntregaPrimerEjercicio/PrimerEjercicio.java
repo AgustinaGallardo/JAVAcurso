e554875cf7ab77dd8ba3477afeac621bb7ca1f77
Agustina Gallardo <agusgallardo275@gmail.com>
2023-01-31 11:51:42 -0300

    TiposDeDatos

--- EntregaPrimerEjercicio/PrimerEjercicio.java
+++ EntregaPrimerEjercicio/PrimerEjercicio.java
@@ -9,12 +9,20 @@ public class PrimerEjercicio {
 		long dni = 34601166l;
 		double peso = 54.3d;
 		boolean estudia = true;
+		char letra = 'a';
+		byte byt = 1; 
+        short num = 10; 
+        float altura = 50.1f;
+		
 	
 		System.out.println("Mi nombre es: "+ nombre);	
 		System.out.println("Tengo " + edad + " años.");
 		System.out.println("Mi numero de dni es " + dni);
 		System.out.println("Mi peso es de: " + peso);
 		System.out.println("Estudias en la actualidad: " + estudia);
+		System.out.println( letra);	
+		System.out.println("Tengo " + altura + " m de alto.");
+		System.out.println(byt + num);
 	}
 		
 	
